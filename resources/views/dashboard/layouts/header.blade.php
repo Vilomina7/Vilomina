9ca04f0738983445786dfacb51b541d038691f7a
vilomina <130976740+vilomina@users.noreply.github.com>
2023-04-25 12:18:29 +0700

    penyesuaian header

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <!-- START PAGE-CONTAINER -->
-<div class="header p-r-0 bg-primary">
+<div class="header p-r-0 bg-success">
     <div class="header-inner header-md-height">
       <a href="#" class="btn-link toggle-sidebar d-lg-none text-white sm-p-l-0 btn-icon-link" data-toggle="horizontal-menu">
         <i class="pg-icon">menu</i>
@@ -23,10 +23,6 @@
           <div class="dropdown-menu dropdown-menu-right profile-dropdown" role="menu">
             <a href="#" class="dropdown-item"><span>Signed in as <br /><b>{{ auth()->user()->name }}</b></span></a>
             <div class="dropdown-divider"></div>
-            {{-- <a href="#" class="dropdown-item">Your Profile</a>
-            <a href="#" class="dropdown-item">Your Activity</a>
-            <a href="#" class="dropdown-item">Your Archive</a>
-            <div class="dropdown-divider"></div> --}}
             <a href="{{ route('profil.edit') }}" class="dropdown-item">Profil</a>
             <a href="/dashboard/password" class="dropdown-item">Ganti Password</a>
             <a href="{{ route('profil.destroy') }}" class="dropdown-item">Hapus Akun</a>
@@ -41,22 +37,22 @@
     </div>
     <div class="bg-white">
       <div class="container">
-        <div class="menu-bar header-sm-height" data-pages-init='horizontal-menu' data-hide-extra-li="4">
+        <div class="menu-bar header-sm-height" data-pages-init='horizontal-menu'>
           <a href="#" class="btn-link header-icon toggle-sidebar d-lg-none" data-toggle="horizontal-menu">
             <i class="pg-icon">close</i>
           </a>
           <ul>
             <li>
-              <a href="/dashboard/posts">Manajemen Penawaran</a>
+              <a href="/dashboard/posts" class="text-decoration-none">Manajemen Penawaran</a>
             </li>
             <li>
-                <a href="{{ route('products.bookmarked') }}">Bookmark</a>
+                <a href="{{ route('products.bookmarked') }}" class="text-decoration-none">Bookmark</a>
             </li>
             <li>
-                <a href="{{ route('users.index') }}">Following</a>
+                <a href="{{ route('users.index') }}" class="text-decoration-none">Following</a>
             </li>
             <li>
-                <a href="/dashboard/notification">Notification</a>
+                <a href="/dashboard/notification" class="text-decoration-none">Notification</a>
             </li>
           </ul>
           <a href="#" class="search-link d-flex justify-content-between align-items-center d-lg-none" data-toggle="search">Tap here to search <i class="pg-search float-right"></i></a>
@@ -64,20 +60,3 @@
       </div>
     </div>
   </div>
-
-
-{{-- <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Vilomina</a>
-    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search">
-    <div class="navbar-nav">
-        <div class="nav-item text-nowrap">
-            <form action="/logout" method="post">
-            @csrf
-            <button type="submit" class="nav-link px-3 bg-dark border-0">Logout <span data-feather="log-out"></span></button>
-          </form>
-        </div>
-    </div>
-</header> --}}
